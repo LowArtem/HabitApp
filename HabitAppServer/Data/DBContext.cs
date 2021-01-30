cda8b1ce936f1bc0b6402b9058f57b6d1f3d979e
LowArtem <artemziharew@gmail.com>
2021-01-30 17:55:29 +0300

    Обновлено подключение Entity Framework Настроен класс DBContext

---
+++ HabitAppServer/Data/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HabitAppServer.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HabitAppDB;Trusted_Connection=True;");
+        }
+    }
+}
