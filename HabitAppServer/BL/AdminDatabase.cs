678d261feda44dc602a45aaaadf72a9cb12c2f74
LowArtem <artemziharew@gmail.com>
2021-02-03 18:13:55 +0300

    Добавлен класс для ручного добавления достижений и наград в БД

---
+++ HabitAppServer/BL/AdminDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HabitAppServer.Data;
+using HabitAppServer.Model.Interfaces;
+
+namespace HabitAppServer.BL
+{
+    /// <summary>
+    /// Класс для ручного добавления Rewards и Achievements в базу данных
+    /// </summary>
+    public class AdminDatabase
+    {
+        private readonly IRepository<Reward> _rewards;
+        private readonly IRepository<Achievement> _achievements;
+
+        public AdminDatabase(IRepository<Reward> rewards, IRepository<Achievement> achievements)
+        {
+            this._rewards = rewards;
+            this._achievements = achievements;
+        }
+
+        /// <summary>Добавить награды в БД</summary>
+        public async Task AddRewards()
+        {
+            foreach (var item in GetRewards())
+            {
+                await _rewards.AddAsync(item);
+            }
+        }
+
+        /// <summary>Добавить достижения в БД</summary>
+        public async Task AddAchievements()
+        {
+            foreach (var item in GetAchievements())
+            {
+                await _achievements.AddAsync(item);
+            }
+        }
+
+
+        private List<Reward> GetRewards()
+        {
+            return new List<Reward>
+            {
+                new Reward
+                {
+                    Category = "",
+                    Description = "",
+                },
+                new Reward
+                {
+                    Category = "",
+                    Description = "",
+                },
+            };
+        }
+
+        private List<Achievement> GetAchievements()
+        {
+            return new List<Achievement>
+            {
+                new Achievement
+                {
+                    Category = "",
+                    Description = "",
+                },
+                new Achievement
+                {
+                    Category = "",
+                    Description = "",
+                },
+            };
+        }
+    }
+}
---
+++ HabitAppServer/Controllers/AdminDatabaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HabitAppServer.BL;
+using HabitAppServer.Data;
+using HabitAppServer.Model.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitAppServer.Controllers
+{
+    /// <summary>
+    /// Контроллер для ручного добавления достижений и наград в БД (можно будет позже написать web-интерфейс для удобства, чтобы не лазить каждый раз в код)
+    /// </summary>
+    /// 
+    [Route("[admin]/[controller]/[action]")]
+    [ApiController]
+    public class AdminDatabaseController : ControllerBase
+    {
+        private readonly AdminDatabase adminDatabase;
+
+        public AdminDatabaseController(IRepository<Reward> rewards, IRepository<Achievement> achievements)
+        {
+            adminDatabase = new AdminDatabase(rewards, achievements);
+        }
+
+
+
+        [HttpGet]
+        public async Task AddNewDataInCode()
+        {
+            await adminDatabase.AddRewards();
+            await adminDatabase.AddAchievements();
+        }
+    }
+}
