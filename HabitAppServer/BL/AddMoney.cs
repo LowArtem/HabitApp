f9ed36d6a36cea0f7f6152bc0ec2d4da9fff1da4
LowArtem <artemziharew@gmail.com>
2021-02-08 16:24:10 +0300

    Добавлен класс AddMoney, структура HabitType Класс для добавления денег на счет пользователя, структура для описания видов привычки

---
+++ HabitAppServer/BL/AddMoney.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HabitAppServer.Data;
+using HabitAppServer.Model.Interfaces;
+
+namespace HabitAppServer.BL
+{
+    /// <summary>
+    /// Добавляет указанное количество денег на счет указанного пользователя
+    /// </summary>
+    public class AddMoney
+    {
+        private readonly IRepository<User> users;
+
+        public AddMoney(IRepository<User> users)
+        {
+            this.users = users;
+        }
+
+        public int? Add(long userId, int money)
+        {
+            var user = users.Get(userId);
+            if (user is null) return null;
+
+            user.Cash += money;
+
+            if (user.Cash < 0) user.Cash = 0;
+
+            users.Update(user);
+
+            return user.Cash;
+        }
+
+        public static int? Add(long userId, int money, IRepository<User> users)
+        {
+            AddMoney addMoney = new AddMoney(users);
+
+            return addMoney.Add(userId, money);
+        }
+    }
+}
---
+++ HabitAppServer/BL/Services/HabitType.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HabitAppServer.BL.Services
+{
+    /// <summary>
+    /// Тип привычки
+    /// </summary>
+    public struct HabitType
+    {
+        /// <summary>Ежедневная привычка</summary>
+        public static readonly string EveryDay = "everyday";
+
+        /// <summary>Еженедельная привычка</summary>
+        public static readonly string EveryWeek = "everyweek";
+
+        /// <summary>Ежемесячная привычка</summary>
+        public static readonly string EveryMonth = "everymonth";
+
+        /// <summary>Ежегодная привычка</summary>
+        public static readonly string EveryYear = "everyyear";
+
+        /// <summary>Обычная (многоразовая) позитивная или негативная привычка</summary>
+        public static readonly string Standart = "standart";
+
+        /// <summary>Привычка одновременно позитивная и негативная (питаться правильно/неправильно)</summary>
+        public static readonly string StandartDouble = "standartdouble";
+
+        /// <summary>Одноразовая привычка = задача</summary>
+        public static readonly string OneTime = "onetime";
+
+
+        /// <summary>
+        /// Проверяет правильность типа привычки
+        /// </summary>
+        /// <param name="type">тип привычки</param>
+        /// <returns></returns>
+        public static bool IsTypeCorrect(string type)
+        {
+            if (type == HabitType.EveryDay) return true;
+            if (type == HabitType.EveryWeek) return true;
+            if (type == HabitType.EveryMonth) return true;
+            if (type == HabitType.EveryYear) return true;
+            if (type == HabitType.Standart) return true;
+            if (type == HabitType.StandartDouble) return true;
+            if (type == HabitType.OneTime) return true;
+
+            return false;
+        }
+    }
+}
